3a3a23c1e32c59a2e77235c938baacc51032459c
perpetually-falling <s2524233@ed.ac.uk>
2025-10-28 15:17:59 +0000

    init hist test

--- test3.py
+++ test3.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # import data
 #xmass = np.loadtxt(sys.argv[1])
-f = open("../ups-15-small.bin","r")
+f = open("C:/Users/K_muk/OneDrive - University of Edinburgh/Physics Year 4/Data Acquisition & Handling/DAH Project/ups-15-small.bin","r")
 datalist = np.fromfile(f,dtype=np.float32)
 
 # number of events
@@ -13,34 +13,51 @@ nevent = int(len(datalist)/6)
 xdata = np.split(datalist,nevent)
 print(xdata[0])
 
-# make list of invariant mass of events
-xmass = []
-for i in range(0,nevent):
-    xmass.append(xdata[i][0])
-#    if i < 10:
-#        print(xmass[i])
 
-# make a list of the transverse momentum of muon pairs
-xpair_trans_mom = []
-for i in range(0,nevent):
-    xpair_trans_mom.append(xdata[i][1])
+# make a finction to extract varialbles from xdata
+def extract_variables(input_data):
 
-# make a list of rapidity of muon pair
-xrapidity = []
-for i in range(0,nevent):
-    xrapidity.append(xdata[i][2])
+    # make list of invariant mass of events
+    xmass = []
+    for i in range(0,nevent):
+        xmass.append(xdata[i][0])
+    #    if i < 10:
+    #        print(xmass[i])
 
-# make a list of momentum of muon mairs
-xpair_mom = []
-for i in range(0,nevent):
-    xpair_mom.append(xdata[i][3])
+    # make a list of the transverse momentum of muon pairs
+    xpair_trans_mom = []
+    for i in range(0,nevent):
+        xpair_trans_mom.append(xdata[i][1])
 
-# make a list of the transverse momentum of first muon
-xfirst_mom = []
-for i in range(0,nevent):
-    xfirst_mom.append(xdata[i][4])
+    # make a list of rapidity of muon pair
+    xrapidity = []
+    for i in range(0,nevent):
+        xrapidity.append(xdata[i][2])
 
-# make a list of the transverse momentum of second muon
-xsecond_mom = []
-for i in range(0,nevent):
-    xsecond_mom.append(xdata[i][5])
+    # make a list of momentum of muon mairs
+    xpair_mom = []
+    for i in range(0,nevent):
+        xpair_mom.append(xdata[i][3])
+
+    # make a list of the transverse momentum of first muon
+    xfirst_mom = []
+    for i in range(0,nevent):
+        xfirst_mom.append(xdata[i][4])
+
+    # make a list of the transverse momentum of second muon
+    xsecond_mom = []
+    for i in range(0,nevent):
+        xsecond_mom.append(xdata[i][5])
+
+    return xmass, xpair_trans_mom, xrapidity, xpair_mom, xfirst_mom, xsecond_mom
+
+xmass, xpair_trans_mom, xrapidity, xpair_mom, xfirst_mom, xsecond_mom = extract_variables(xdata)
+
+# plot histograms of variables
+plt.hist(xmass, bins=100, histtype='step', label='Invariant Mass of Muon Pairs')
+plt.xlabel('Invariant Mass (GeV/c^2)')
+plt.ylabel('Number of Events')
+plt.xlim(xmass[0], xmass[-1])
+plt.title('Histogram of Invariant Mass of Muon Pairs')
+plt.legend()
+plt.show()
