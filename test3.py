79d779110c13cdd74da8942acf132e50edaa4996
perpetually-falling <s2524233@ed.ac.uk>
2025-10-28 14:57:11 +0000

    retry

--- test3.py
+++ test3.py
@@ -17,7 +17,30 @@ print(xdata[0])
 xmass = []
 for i in range(0,nevent):
     xmass.append(xdata[i][0])
-    if i < 10:
+#    if i < 10:
         print(xmass[i])
 
+# make a list of the transverse momentum of muon pairs
+xpair_trans_mom = []
+for i in range(0,nevent):
+    xpair_trans_mom.append(xdata[i][1])
 
+# make a list of rapidity of muon pair
+xrapidity = []
+for i in range(0,nevent):
+    xrapidity.append(xdata[i][2])
+
+# make a list of momentum of muon mairs
+xpair_mom = []
+for i in range(0,nevent):
+    xpair_mom.append(xdata[i][3])
+
+# make a list of the transverse momentum of first muon
+xfirst_mom = []
+for i in range(0,nevent):
+    xfirst_mom.append(xdata[i][4])
+
+# make a list of the transverse momentum of second muon
+xsecond_mom = []
+for i in range(0,nevent):
+    xsecond_mom.append(xdata[i][5])
